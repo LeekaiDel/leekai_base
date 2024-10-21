542d51e5579f0af4515e7fc5c6fe2f9ad339eb0a
Leekai <leekai.dev@gmail.com>
2024-10-22 00:40:30 +0300

    добавлены певые команды в скрипты, логики пока нет

---
+++ sys_scripts/base_configure_linux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install git terminator nmap mc net-tools vim ssh build-essential 
+
+git config --global user.name "Leekai"
+git config --global user.email leekai.dev@gmail.com
+
+sudo snap install code --classic
