666c00c7229d6bca2bf11bc60fb7ecf6a2545dd0
Leekai <leekai.dev@gmail.com>
2026-01-04 04:03:09 +0300

    fixed base conf for fedora

--- sys_scripts/base_conf_fedora.sh
+++ sys_scripts/base_conf_fedora.sh
@@ -4,4 +4,8 @@ sudo dnf upgrade --refresh -y
 git config --global user.name "Leekai"
 git config --global user.email leekai.dev@gmail.com
 
+# Установите RPM Fusion (free и non-free)
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 sudo dnf install -y --skip-unavailable nmap mc net-tools vim terminator
