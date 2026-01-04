15c8c34fa284bc1c875a7e993b3fdc0434c03596
Leekai <leekai.dev@gmail.com>
2026-01-04 16:42:53 +0300

    Add install ros2 Jazzy for Fedora

---
+++ sys_scripts/install_ros_fedora.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/sh
+# Создайте рабочую директорию
+mkdir -p ~/ros2_jazzy/src
+cd ~/ros2_jazzy
+
+# Скачайте исходники ROS 2 Jazzy
+wget https://raw.githubusercontent.com/ros2/ros2/jazzy/ros2.repos
+vcs import src < ros2.repos
+
+# Установите системные зависимости
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+# Основные переменные для сборки RPM в Fedora
+export RPM_ARCH=$(uname -m)
+export RPM_PACKAGE_NAME=ros2
+export RPM_PACKAGE_VERSION=2.0
+export RPM_PACKAGE_RELEASE=1.fc$(rpm -E %fedora)
+export RPM_PACKAGE_VENDOR="ROS 2 Team"
+
+# Или минимальный набор
+#export RPM_PACKAGE_RELEASE=1
+#export RPM_ARCH=x86_64
+
+
+# Соберите ROS 2
+colcon build --symlink-install
+
+# Добавьте в .bashrc
+echo "source ~/ros2_jazzy/install/local_setup.bash" >> ~/.bashrc
+source ~/.bashrc
