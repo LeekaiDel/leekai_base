105e5b95af2dc8e920bb80d6ebdad9f0df2abfbd
Leekai <leekai.dev@gmail.com>
2026-01-04 04:07:10 +0300

    fixed base conf for fedora, add install vscode

--- sys_scripts/base_conf_fedora.sh
+++ sys_scripts/base_conf_fedora.sh
@@ -9,3 +9,13 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf install -y --skip-unavailable nmap mc net-tools vim terminator
+
+# Импортируйте ключ Microsoft GPG
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Добавьте репозиторий VS Code
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# Обновите кэш и установите
+sudo dnf check-update -y
+sudo dnf install code -y
