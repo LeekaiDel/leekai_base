43a032ca6d915daa4ff73667a7d3182a2a540613
Leekai <leekai.dev@gmail.com>
2026-01-08 18:54:31 +0300

    fix

--- sys_scripts/base_conf_fedora.sh
+++ sys_scripts/base_conf_fedora.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-sudo dnf upgrade --refresh -y
-
+# Проверяем был ли запущен этот скрипт от имени администратора
+[ $EUID -ne 0 ] && echo "Запустите: sudo $0" && exit 1
+# Сразу конфигурим git
 git config --global user.name "Leekai"
 git config --global user.email leekai.dev@gmail.com
 
-# Установите RPM Fusion (free и non-free)
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf upgrade --refresh -y
 
-sudo dnf install -y --skip-unavailable nmap htop mc net-tools vim terminator
+# Установите RPM Fusion (free и non-free)
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y --skip-unavailable nmap htop mc net-tools vim terminator
 
 # Импортируйте ключ Microsoft GPG
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # Добавьте репозиторий VS Code
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # Обновите кэш и установите
-sudo dnf check-update -y
-sudo dnf install code -y
+dnf check-update -y
+dnf install code -y
